48c6449abd030cd4185ebfb4edd89581bf50029d
Luvchopra19 <luvchopra12@gmail.com>
2026-07-13 16:30:49 +0530

    done

--- JavaWeb-Project/src/main/java/com/rays/model/UserModel.java
+++ JavaWeb-Project/src/main/java/com/rays/model/UserModel.java
@@ -92,10 +92,8 @@ public class UserModel {
 
 	public void delete(UserBean bean) throws Exception {
 
-		Class.forName("com.mysql.cj.jdbc.Driver");
-
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rays", "root", "root");
-
+		Connection conn = JDBCDataSource.getConnection();
+		
 		PreparedStatement pstmt = conn.prepareStatement("delete from st_user where id = ?");
 
 		pstmt.setInt(1, bean.getId());
@@ -136,10 +134,8 @@ public class UserModel {
 
 	public UserBean authenticate(String login, String password) throws Exception {
 
-		Class.forName("com.mysql.cj.jdbc.Driver");
-
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rays", "root", "root");
-
+		Connection conn = JDBCDataSource.getConnection();
+		
 		PreparedStatement pstmt = conn.prepareStatement("select * from st_user where login = ? and passward = ?");
 
 		pstmt.setString(1, login);
@@ -165,10 +161,8 @@ public class UserModel {
 
 	public UserBean findByPk(int id) throws Exception {
 
-		Class.forName("com.mysql.cj.jdbc.Driver");
-
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rays", "root", "root");
-
+		Connection conn = JDBCDataSource.getConnection();
+		
 		PreparedStatement pstmt = conn.prepareStatement("select * from st_user where id = ?");
 
 		pstmt.setInt(1, id);
@@ -193,10 +187,8 @@ public class UserModel {
 
 	public List<UserBean> search(UserBean bean, int pageNo, int pageSize) throws Exception {
 
-		Class.forName("com.mysql.cj.jdbc.Driver");
-
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rays", "root", "root");
-
+		Connection conn = JDBCDataSource.getConnection();
+		
 		StringBuffer sql = new StringBuffer("select * from st_user where 1=1");
 
 		if (bean != null) {
